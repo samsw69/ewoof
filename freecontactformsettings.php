b857cabef313c3f063131a36e041646516225d92
samsw69 <sam@housenhome.co.uk>
2017-06-19 23:14:22 +0100

    50% page 1 consent form created in php

---
+++ freecontactformsettings.php
@@ -0,0 +1,10 @@
+<?php
+
+$email_to = "info@ewoof.co.uk"; // your email address
+$email_subject = "eWoof - Contact Form Message"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+?>
